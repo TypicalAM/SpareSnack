d579a7bdc02f9a3f3a71030aeec0ecff2d929ab1
Adam Piaseczny <cypis183@gmail.com>
2022-05-27 00:12:28 +0200

    Model change

--- manage.py
+++ manage.py
@@ -3,9 +3,10 @@
 import os
 import sys
 
+
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sparesnack.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sparesnack.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -14,8 +15,9 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
