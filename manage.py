cbce4160a14484c76527bf75788aa829b99456ba
Adam Piaseczny <cypis183@gmail.com>
2022-08-15 11:01:03 +0200

    Added debugging

--- manage.py
+++ manage.py
@@ -16,6 +16,15 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
 
+    from django.conf import settings
+
+    if settings.DEBUG:
+        if os.environ.get("RUN_MAIN") or os.environ.get("WERKZEUG_RUN_MAIN"):
+            import debugpy
+
+            debugpy.listen(("0.0.0.0", 3000))
+            print("Attached!")
+
     execute_from_command_line(sys.argv)
 
 
