92da757e9283d68cb12220c3c22ec1b19a417c9d
Adam Piaseczny <cypis183@gmail.com>
2022-08-30 14:12:26 +0200

    Refactored users models.py

--- users/models.py
+++ users/models.py
@@ -11,22 +11,21 @@ class Profile(models.Model):
     """A profile for the user to change his preferences"""
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    is_metric = models.BooleanField(default=True)
+    fats = models.FloatField(default=0)
+    protein = models.FloatField(default=0)
+    carbs = models.FloatField(default=0)
     image = ResizedImageField(
         size=[160, 160],
         default="users/default_avatar.png",
         upload_to=UploadAndRename("users/profile_pictures"),
     )
-    is_metric = models.BooleanField(default=True)
 
-    fats = models.FloatField(default=0)
-    protein = models.FloatField(default=0)
-    carbs = models.FloatField(default=0)
+    def __str__(self):
+        """Additional data for debugging"""
+        return f"{self.user.username}'s Profile"
 
     def save(self, *args, **kwargs):
         """Clean up after the old image if we have a new one"""
         image_clean_up(self)
         return super().save(*args, **kwargs)
-
-    def __str__(self):
-        """Additional data for debugging"""
-        return f"{self.user.username}'s Profile"
