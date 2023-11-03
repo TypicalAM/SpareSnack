e36c497f714f50f31da4f19f7bc5622c79bff4e1
TypicalAM <tpam_piaseczny@protonmail.com>
2023-11-04 00:28:59 +0100

    Working dev docker compose

--- src/core/settings.py
+++ src/core/settings.py
@@ -145,7 +145,7 @@ STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_URL = "media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
+MEDIA_ROOT = "/media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
