778081a3a0ec371ecf4eaf04694b3408f51f3928
TypicalAM <tpam_piaseczny@protonmail.com>
2025-03-07 13:34:02 +0100

    Trusted origins typo

--- src/core/settings.py
+++ src/core/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = os.environ.get(
     "DJANGO_ALLOWED_HOSTS", default="localhost"
 ).split(" ")
 
-ALLOWED_HOSTS = os.environ.get(
+CSRF_TRUSTED_ORIGINS = os.environ.get(
     "DJANGO_CSRF_TRUSTED_ORIGINS", default="localhost"
 ).split(" ")
 
